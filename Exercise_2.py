9ba120f4601f518cd90d0ec9d4b6d680a9a2cac3
mrkormaks <38488121+mrkormaks@users.noreply.github.com>
2020-09-04 11:41:05 +0300

    Update Exercise_2.py

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,5 +1,18 @@
 def odometer(N):
-    distance = 1
-    b = N[::2]
-    distance = sum(b)
-    return distance
+	distance = 0
+	list_1 = []
+	list_2 = []
+	list_3 = []
+	for i in range(len(N)):
+		if i % 2 == 0:
+			list_1.append(N[i])
+		if i % 2 != 0:
+			list_2.append(N[i])
+	for i in range(len(list_2)):
+		if i == 0:
+			list_3.append(list_2[i])
+		else:
+			list_3.append(list_2[i] - list_2[i-1])
+	for i in range(len(list_1)):
+		distance += list_1[i] * list_3[i]
+	return(distance)
